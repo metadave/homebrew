a1ed938eb7ff7b3069e01e687f4bc15b4f9e413c
Dave Parfitt <dparfitt@basho.com>
2013-12-25 21:46:24 -0500

    removed generated comments, fixed test

--- Library/Formula/erln8.rb
+++ Library/Formula/erln8.rb
@@ -1,9 +1,4 @@
 require 'formula'
-
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Erln8 < Formula
   homepage ''
   url 'https://github.com/metadave/erln8/archive/erln8-0.9.1.tar.gz'
@@ -20,14 +15,6 @@ class Erln8 < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test erln8`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "--version"`.
-    system "erln8 --help"
+    system "#{bin}/erln8 --help"
   end
 end
